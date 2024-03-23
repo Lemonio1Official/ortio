833dc1549eed188a263aff1b01f11ad0e7db93ad
Eugene <feylpixel@gmail.com>
2024-03-23 16:19:36 +0200

    init server

---
+++ server/index.ts
@@ -0,0 +1,37 @@
+import WebSocket from "ws"
+import User from "./src/User";
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    const user = new User(ws)
+    console.log(`New client connected ${user.id}`);
+
+    ws.on('message', (message) => {
+        const { action, data } = JSON.parse(message.toString())
+        switch (action) {
+            case "join":
+                user.join(data)
+                break
+            case "leave":
+                user.leave()
+                break
+            case "step":
+                user.step(data)
+                break
+            case "start":
+                user.start()
+                break
+        }
+        // wss.clients.forEach((client) => {
+        //     if (client.readyState === WebSocket.OPEN) {
+        //         client.send(message);
+        //     }
+        // });
+    });
+
+    ws.on('close', () => {
+        console.log(`Client disconnected ${user.id}`);
+        user.disconnect()
+    });
+});
---
+++ server/src/User.ts
@@ -0,0 +1,56 @@
+import uniqid from "uniqid"
+
+class User {
+    static users: User[] = []
+
+    ws: WebSocket
+    id: string
+    joined = ""
+
+    constructor(ws: WebSocket) {
+        this.id = uniqid.time()
+        this.ws = ws
+        User.users.push(this)
+        ws.send(JSON.stringify({ action: "connect", data: this.id }))
+    }
+
+    join(id: string) {
+        const user = User.users.find(i => i.id == id)
+        if (this.id !== id && user) {
+            this.joined = id
+            user.joined = this.id
+            this.ws.send(JSON.stringify({ action: "join", data: id }))
+            user.ws.send(JSON.stringify({ action: "join", data: this.id }))
+        } else {
+            this.ws.send(JSON.stringify({ action: "join", data: "" }))
+        }
+    }
+    leave() {
+        const user = User.users.find(i => i.id == this.joined)
+        if (user) {
+            user.joined = ""
+            user.ws.send(JSON.stringify({ action: "leave", data: "" }))
+        }
+        this.joined = ""
+        this.ws.send(JSON.stringify({ action: "leave", data: "" }))
+    }
+    step(data: any) {
+        const user = User.users.find(i => i.id == this.joined)
+        if (user) user.ws.send(JSON.stringify({ action: "step", data }))
+    }
+    start() {
+        const rand = Math.floor(Math.random() * 2)
+        const turn = rand == 0
+        this.ws.send(JSON.stringify({ action: "start", data: turn }))
+        const user = User.users.find(i => i.id == this.joined)
+        if (user) user.ws.send(JSON.stringify({ action: "start", data: !turn }))
+    }
+
+    disconnect() {
+        User.users = User.users.filter(i => i.id !== this.id)
+        const user = User.users.find(i => i.id == this.joined)
+        if (user) user.leave()
+    }
+}
+
+export default User
